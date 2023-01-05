0a55fcda2c0307227bbabf4912ff71c3b2834c5d
DiegoSalas27 <dominicsc2hs@gmail.com>
2023-01-05 13:37:18 -0600

    test: add IngredientRepository bean

--- src/test/java/tacos/HomeControllerTest.java
+++ src/test/java/tacos/HomeControllerTest.java
@@ -6,9 +6,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import tacos.repository.IngredientRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest // <1>
@@ -16,6 +18,9 @@ public class HomeControllerTest {
 
 	@Autowired
 	private MockMvc mockMvc; // <2>
+	
+	@MockBean
+	private IngredientRepository ingredientRepository;
 
 	@Test
 	public void testHomePage() throws Exception {
