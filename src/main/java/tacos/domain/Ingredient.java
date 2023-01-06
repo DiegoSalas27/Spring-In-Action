79e945f4a9954a789d90261334eae2d8b40adc32
DiegoSalas27 <dominicsc2hs@gmail.com>
2023-01-06 15:17:11 -0600

    feat: preloading data with commandLineRunner

--- src/main/java/tacos/domain/Ingredient.java
+++ src/main/java/tacos/domain/Ingredient.java
@@ -1,6 +1,7 @@
 package tacos.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.domain.Persistable;
 import org.springframework.data.relational.core.mapping.Table;
 
@@ -15,15 +16,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 public class Ingredient implements Persistable<String> {
 	@Id
-	private final String id;
-	private final String name;
-	private final Type type;
+	private String id;
+	
+	private String name;
+	private Type type;
 
-	@Override
-	public boolean isNew() {
-		return true;
-	}
-
+    @Override
+    public boolean isNew() {
+        return true;
+    }
+    
 	public enum Type {
 		WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
 	}
