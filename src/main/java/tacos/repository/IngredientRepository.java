22887fba2f56e71f8be12651bf9377746dc75839
DiegoSalas27 <dominicsc2hs@gmail.com>
2023-01-05 19:07:58 -0600

    feat: annotating the domain for persistence

--- src/main/java/tacos/repository/IngredientRepository.java
+++ src/main/java/tacos/repository/IngredientRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import tacos.domain.Ingredient;
 
 public interface IngredientRepository extends CrudRepository<Ingredient, String> {
-	
+
 }
-
-
