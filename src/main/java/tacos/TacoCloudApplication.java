545963764d210827436bd71ae7fdad3349281de8
DiegoSalas27 <dominicsc2hs@gmail.com>
2023-01-11 14:13:58 -0600

    feat: update configuration class to add tacos when application starts

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -1,14 +1,8 @@
 package tacos;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import tacos.domain.Ingredient;
-import tacos.domain.Ingredient.Type;
-import tacos.repository.IngredientRepository;
 
 @SpringBootApplication
 @EnableGlobalMethodSecurity(prePostEnabled = true)
