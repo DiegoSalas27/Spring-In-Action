8a27c9eb7aeab8754405921ebeeef2f16f469b01
DiegoSalas27 <dominicsc2hs@gmail.com>
2023-01-11 11:15:07 -0600

    feat: list orders from logged in user

--- src/main/java/tacos/repository/OrderRepository.java
+++ src/main/java/tacos/repository/OrderRepository.java
@@ -1,8 +1,13 @@
 package tacos.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import tacos.domain.TacoOrder;
+import tacos.domain.User;
 
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+	List<TacoOrder> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 }
