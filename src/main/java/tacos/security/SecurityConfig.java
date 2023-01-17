049b020f7490349468749085bc600fbdc659ee81
DiegoSalas27 <dominicsc2hs@gmail.com>
2023-01-16 22:18:04 -0600

    style: format code

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -1,5 +1,4 @@
 package tacos.security;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,7 +9,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
 import tacos.domain.User;
 import tacos.repository.UserRepository;
 
